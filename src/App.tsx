2e7b753e2895eabbe9d51db3530c6a3422a1fa17
Silas Martins <silas.martins2041@gmail.com>
2023-04-08 22:10:19 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {  BrowserRouter as Router } from 'react-router-dom';
+
+import { Routes } from './routes';
+
+import GlobalStyle from './styles/global';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+        <Router>
+          <Routes />
+        </Router>
+    </>
+  )
+}
